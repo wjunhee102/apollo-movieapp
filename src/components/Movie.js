bc18f82bc5b897dcccbb8525ade7c1b2a22d4761
junhee <wjunhee102@gmail.com>
2020-07-09 15:29:20 +0900

    detail 컴포넌트 추가

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default ({id, img, title}) => {
+  console.log(typeof id);
   return (
     <div>
       <Link to={`/detail/${id}`}>
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -4,9 +4,11 @@ import { useParams } from  "react-router-dom";
 import { gql } from "apollo-boost";
 import { useQuery } from "@apollo/react-hooks";
 
+import styled from "styled-components";
+
 const GET_MOVIE = gql`
-  query getById($id: Int) {
-    movie(id: 19019) {
+  query getById($id: Int!) {
+    movie(id: $id) {
       id
       title
       medium_cover_image
@@ -14,15 +16,67 @@ const GET_MOVIE = gql`
   }
 `;
 
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+
+const Column = styled.div`
+  margin-left: 10px;
+  width: 50%;
+`;
+
+const Title = styled.h1`
+  font-size: 65px;
+  margin-bottom: 15px;
+`;
+
+const Subtitle = styled.h4`
+  font-size: 35px;
+  margin-bottom: 10px;
+`;
+
+const Description = styled.p`
+  font-size: 28px;
+`;
+
+const Poster = styled.div`
+  width: 25%;
+  height: 60%;
+  background-color: transparent;
+  background-image: url(${props => props.bg});
+  background-size: cover;
+  background-position: center center;
+`;
+
 export default () => {
   const { id } = useParams();
 
-  const { data } = useQuery(GET_MOVIE);
-  console.log(id);
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: { id: parseInt(id) }
+  });
+
+  console.log(loading, data);
 
   return (
-    <div>
-      {data?.title}
-    </div>
+    <Container>
+      <Column>
+        <Title>
+          {loading
+            ? "Loading..."
+            : `${data.movie[0].title} ${data.movie[0].isLiked ? "💖" : "😞"}`}
+        </Title>
+        <Subtitle>
+          {data?.movie[0]?.language} · {data?.movie[0]?.rating}
+        </Subtitle>
+        <Description>{data?.movie[0]?.description_intro}</Description>
+      </Column>
+      <Poster bg={data?.movie[0]?.medium_cover_image}></Poster>
+    </Container>
   )
 };
